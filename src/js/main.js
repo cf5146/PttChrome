92022c3854a8c90d54ec121b4adfc5e308bd64e3
cf5146 <cf5146@gmail.com>
2026-04-17 15:05:11 -0500

    Fix startup black screen in developer mode

--- src/js/main.js
+++ src/js/main.js
@@ -3,36 +3,44 @@ import { setupI18n } from './i18n';
 import { getQueryVariable } from './util';
 import { readValuesWithDefault } from '../components/ContextMenu/PrefModal';
 
+function showDeveloperModeAlert() {
+  if (!process.env.DEVELOPER_MODE) {
+    return;
+  }
+
+  import('../components/DeveloperModeAlert')
+    .then(({ DeveloperModeAlert }) => {
+      const container = document.getElementById('reactAlert');
+      const onDismiss = () => {
+        ReactDOM.unmountComponentAtNode(container);
+      };
+
+      ReactDOM.render(
+        <DeveloperModeAlert onDismiss={onDismiss} />,
+        container
+      );
+    })
+    .catch(error => {
+      console.error('showDeveloperModeAlert failed:', error);
+    });
+}
+
 function startApp() {
   setupI18n();
 
   const app = new App();
 
-  (process.env.DEVELOPER_MODE ? import('../components/DeveloperModeAlert')
-    .then(({DeveloperModeAlert}) => new Promise((resolve, reject) => {
-      const container = document.getElementById('reactAlert')
-      const onDismiss = () => {
-        ReactDOM.unmountComponentAtNode(container)
-        resolve()
-      }
-      ReactDOM.render(
-        <DeveloperModeAlert onDismiss={onDismiss} />,
-        container
-      )
-    })) : Promise.resolve()
-  ).then(() => {
-    // connect.
-    app.connect(
-      process.env.ALLOW_SITE_IN_QUERY && getQueryVariable('site')
-      || process.env.DEFAULT_SITE);
-    // TODO: Call onSymFont for font data when it's implemented.
-    console.log("load pref from storage");
-    app.onValuesPrefChange(readValuesWithDefault());
-    app.setInputAreaFocus();
-    $('#BBSWindow').show();
-    //$('#sideMenus').show();
-    app.onWindowResize();
-  })
+  showDeveloperModeAlert();
+
+  // connect.
+  app.connect(
+    process.env.ALLOW_SITE_IN_QUERY && getQueryVariable('site')
+    || process.env.DEFAULT_SITE);
+  console.log("load pref from storage");
+  app.onValuesPrefChange(readValuesWithDefault());
+  app.setInputAreaFocus();
+  $('#BBSWindow').show();
+  app.onWindowResize();
 }
 
 function loadTable(url) {
@@ -48,9 +56,9 @@ function loadResources() {
     loadTable(require('../conv/b2u_table.bin')),
     loadTable(require('../conv/u2b_table.bin'))
   ]).then(function(binData) {
-    window.lib = window.lib || {};
-    window.lib.b2uArray = new Uint8Array(binData[0]);
-    window.lib.u2bArray = new Uint8Array(binData[1]);
+    globalThis.lib = globalThis.lib || {};
+    globalThis.lib.b2uArray = new Uint8Array(binData[0]);
+    globalThis.lib.u2bArray = new Uint8Array(binData[1]);
     $(document).ready(startApp);
   }, function(e) {
     console.log('loadResources failed: ' + e);
